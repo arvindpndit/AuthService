77e16db2dca5c7600754ed97974c58c28e29e7f8
arvindpndit <arvindpandit9808@gmail.com>
2023-08-28 09:04:57 +0530

    added role to user in user_roles table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
 //const db = require("./models/index");
+const { User, Role } = require("./models/index");
 
 const app = express();
 
@@ -13,6 +14,9 @@ const getServerStarted = () => {
   app.use("/api", apiRoutes);
 
   app.listen(PORT, async () => {
+    const u1 = await User.findByPk(1);
+    const r1 = await Role.findByPk(1);
+    u1.addRole(r1);
     //db.sequelize.sync({ alter: true });
     console.log(`Server is running on PORT ${PORT}`);
   });
